8dc5b70966db77202b96e6f3a8f6b080fde87f47
Nandini R M <163501639+nandini219@users.noreply.github.com>
2024-05-14 23:28:34 +0530

    Add files via upload

---
+++ URLShortenerService.js
@@ -0,0 +1,28 @@
+﻿const urlList = {};
+
+function encodeURL() {
+  let longURL = document.getElementById("url").value;
+  if (!longURL) {
+    alert("please enter URL");
+	return;
+  }  
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const codeLength = 4; 
+  let shortCode = '';
+  for (let i = 0; i < codeLength; i++) {
+	const randomIndex = Math.floor(Math.random() * characters.length);
+	shortCode += characters[randomIndex];
+  }  
+  urlList[shortCode] = longURL;  
+  document.getElementById("urldata").innerText = shortCode;
+}
+
+function decodeURL() {
+  let shortURL = document.getElementById("urldata").innerText;
+  const shortCode = shortURL.substring(shortURL.lastIndexOf('/') + 1);
+  const longURL = urlList[shortCode];
+  if (!longURL) {
+    alert('Short URL not found');
+  }
+  document.getElementById("urldata").innerText = longURL;
+}
